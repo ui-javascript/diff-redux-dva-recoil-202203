641c9bbd9bb72bf1d374ce9a10bb7094711ef227
luo0412 <1095847440@qq.com>
2022-03-22 14:26:35 +0800

    Initial commit

---
+++ __demo/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h2>Demo Page</h2>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
---
+++ build.config.js
@@ -0,0 +1,26 @@
+const { getEntry } = require('./mpa.utils')
+const { CONFIG } = require('./mpa.config')
+
+const mpaEntries = getEntry(CONFIG.entry);
+console.log(mpaEntries.entry)
+
+module.exports = {
+  define: {
+    env: process.env.NODE_ENV,
+  },
+  minify: 'esbuild',
+  // entry: {
+  //   demo: '_demo/index',
+  // },
+  entry: mpaEntries.entry,
+  publicPath: './',
+  outputDir: 'docs',
+  plugins: [
+    [
+      'build-plugin-ignore-style',
+      {
+        libraryName: 'antd',
+      },
+    ],
+  ],
+};
